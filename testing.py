28bbefdcf3beb64e1a58102a30e3ec4a46a6830b
Ajharnoor <ajharnoor@hotmail.co.uk>
2016-10-10 15:26:55 +0100

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,7 @@
+#x will be the number that i user imputs
+x=int(input("enter a number "))
+#x will print the number that the user typed in
+#the y value will now add the number 2 to the number thats been inputted
+y=x+2
+#it will now show the added number as the y value
+print(y)
