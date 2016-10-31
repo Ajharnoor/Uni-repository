9691cc34f6ed73b2dfa478906568a5102464be0a
Ajharnoor <ajharnoor@hotmail.co.uk>
2016-10-31 15:22:48 +0000

    Add files via upload

---
+++ data files.py
@@ -0,0 +1,13 @@
+a = [66.25, 333, 333, 2, 1234.5]
+print("A is", a)
+
+a.insert(2, -1)
+a.append(333)
+print(a)
+a.index(333) #returns the index in the list of the first item which value is x
+a.remove(333)
+print(a)
+a.reverse ()
+print (a)
+a.sort()
+print (a)
---
+++ loop.py
@@ -0,0 +1,4 @@
+nums = []
+for x in [1,2,3,4,5,6,7,8,9,10]: #adding numbers for a loop
+    nums.append(x**2) #x is square rooting the numbers
+print(nums)
