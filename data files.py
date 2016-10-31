d0d326b2bcb21430a238baf0b91204e9daf1ea75
Ajharnoor <ajharnoor@hotmail.co.uk>
2016-10-31 15:25:32 +0000

    Update data files.py

--- data files.py
+++ data files.py
@@ -1,13 +1,13 @@
 a = [66.25, 333, 333, 2, 1234.5]
 print("A is", a)
 
-a.insert(2, -1)
+a.insert(2, -1) #inserting a value in the list 
 a.append(333)
 print(a)
 a.index(333) #returns the index in the list of the first item which value is x
-a.remove(333)
+a.remove(333) #removing a value
 print(a)
-a.reverse ()
+a.reverse () # reversing the list
 print (a)
-a.sort()
+a.sort() #sorting the list from lowest to highest
 print (a)
